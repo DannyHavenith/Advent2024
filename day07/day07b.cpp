e11e5a9ac81bbf7026d3b1d6267b9d57f46d5684
Danny <dh.git@havenith.net>
2026-01-21 23:51:26 +0100

    Solve 7b

---
+++ day07/day07b.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <ranges>
+#include <regex>
+#include <span>
+#include <string>
+
+#include "../timer.h"
+
+namespace {
+    using Number = std::uint64_t;
+
+    bool Solvable( const Number result, const Number soFar, std::span<std::string> elements)
+    {
+        if (soFar > result) return false;
+        if (elements.empty()) return result == soFar;
+
+        const auto front = std::stoull( elements.front());
+
+        const auto multiplied = soFar * front;
+        const auto added = soFar + front;
+        const auto concatenated = std::stoull( std::to_string( soFar) + elements.front());
+
+        // try all combinations, but be aware of overflow.
+        return
+            (multiplied >= soFar and Solvable( result, multiplied, elements.subspan<1>()))
+            or (added   > soFar and Solvable( result, added, elements.subspan<1>()))
+            or (concatenated > soFar and Solvable( result, concatenated, elements.subspan<1>()));
+    }
+
+    bool Solvable( const Number result, std::span<std::string> elements)
+    {
+        return Solvable( result, std::stoull( elements.front()), elements.subspan<1>());
+    }
+}
+
+int main()
+{
+    Timer _;
+    std::ifstream input{"input07.txt"};
+    std::string line;
+
+    const std::regex equationPattern{"(\\d+): (.*)"};
+    std::smatch match;
+
+    Number sum = 0;
+    while (getline( input, line) and std::regex_match( line, match, equationPattern))
+    {
+        std::vector< std::string> numbers;
+        const auto result = std::stoull( match[1]);
+        for (const auto &numString : std::views::split( std::string_view{match[2].first, match[2].second}, ' '))
+        {
+            numbers.emplace_back( numString.begin(), numString.end());
+        }
+
+        if (Solvable( result, numbers)) sum += result;
+    }
+
+    std::cout << "sum: " << sum << '\n';
+}
